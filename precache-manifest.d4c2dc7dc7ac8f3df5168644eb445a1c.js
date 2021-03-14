2cdaf6cf311e3214b5336668173af185146ca1da
Can Çitoğlu <cancitoglu@gmail.com>
2021-03-14 17:27:14 +0200

    add website

---
+++ precache-manifest.d4c2dc7dc7ac8f3df5168644eb445a1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a41f7971b38cae8511f52d87519a3d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4df60e5b190ac56c0e4",
+    "url": "/static/css/main.82aff961.chunk.css"
+  },
+  {
+    "revision": "376b4add28a67cac06a3",
+    "url": "/static/js/2.23fa7cf3.chunk.js"
+  },
+  {
+    "revision": "833dfa5ee128726e71aece8b41cdeb34",
+    "url": "/static/js/2.23fa7cf3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4df60e5b190ac56c0e4",
+    "url": "/static/js/main.bb1ee05d.chunk.js"
+  },
+  {
+    "revision": "af8b4e98f711197446f1",
+    "url": "/static/js/runtime-main.d6165c3a.js"
+  }
+]);
